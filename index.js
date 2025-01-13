a4686eeef60693dc8ffc1a8de13a60f2ee9f7380
Phong <phong1572005@gmail.com>
2025-01-13 23:36:00 +0700

    replace data storage as a postgresql db

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from 'pg';
 
 const app = express();
 const port = 3000;
+
+const { Pool } = pg;
+const db = new Pool({
+  user: 'postgres',
+  password: '15072005',
+  host: 'localhost',
+  port: 5433,
+  database: 'Tasks',
+});
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -12,22 +23,43 @@ let items = [
   { id: 2, title: "Finish homework" },
 ];
 
-app.get("/", (req, res) => {
-  res.render("index.ejs", {
-    listTitle: "Today",
-    listItems: items,
-  });
+app.get("/", async (req, res) => {
+  try {
+    const result = await db.query("SELECT id, title FROM items WHERE time = CURRENT_DATE");
+    const items = result.rows
+    res.render("index.ejs", {
+      listTitle: "Today",
+      listItems: items,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json("Internal server error");
+  }
 });
 
 app.post("/add", (req, res) => {
-  const item = req.body.newItem;
-  items.push({ title: item });
+  console.log(req.body);
+  const newItem = req.body.newItem;
+  try {
+    const result = db.query("INSERT INTO items (title) VALUES ($1)", [newItem]);
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+    res.status(500).json("Internal server error");
+  }
+  items.push({ title: newItem });
   res.redirect("/");
 });
 
-app.post("/edit", (req, res) => {});
+app.post("/edit", (req, res) => {
+  console.log(req.body);
+  const updatedItemId = req.body.updatedItemId;
+  const updatedItemTitle = req.body.updatedItemTitle;
+});
 
-app.post("/delete", (req, res) => {});
+app.post("/delete", (req, res) => {
+  console.log(req.body);
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
--- queries.sql
+++ queries.sql
@@ -1,6 +1,7 @@
 CREATE TABLE items (
   id SERIAL PRIMARY KEY,
-  title VARCHAR(100) NOT NULL
+  title VARCHAR(100) NOT NULL,
+  time DATE default CURRENT_DATE
 );
 
 INSERT INTO items (title) VALUES ('Buy milk'), ('Finish homework');
