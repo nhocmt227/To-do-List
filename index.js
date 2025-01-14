43121c0252ff306032a87e35fb79a2d24c6994af
Phong <phong1572005@gmail.com>
2025-01-14 10:41:06 +0700

    complete backend

--- index.js
+++ index.js
@@ -25,8 +25,9 @@ let items = [
 
 app.get("/", async (req, res) => {
   try {
-    const result = await db.query("SELECT id, title FROM items WHERE time = CURRENT_DATE");
-    const items = result.rows
+    const result = await db.query("SELECT id, title FROM items WHERE time = CURRENT_DATE ORDER BY id");
+    const items = result.rows;
+    console.log(items);
     res.render("index.ejs", {
       listTitle: "Today",
       listItems: items,
@@ -37,28 +38,49 @@ app.get("/", async (req, res) => {
   }
 });
 
-app.post("/add", (req, res) => {
+app.post("/add", async (req, res) => {
   console.log(req.body);
-  const newItem = req.body.newItem;
   try {
-    const result = db.query("INSERT INTO items (title) VALUES ($1)", [newItem]);
+    const newItem = req.body.newItem;
+    const result = await db.query("INSERT INTO items (title) VALUES ($1)", [newItem]);
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+    if (error.code == 22001) {
+      res.status(500).json("Value too long, maximum 100 characters");
+    } else {
+      res.status(500).json("Internal server error");
+    }
+  }
+});
+
+app.post("/edit", async (req, res) => {
+  console.log(req.body);
+  try {
+    const updatedItemId = req.body.updatedItemId;
+    const updatedItemTitle = req.body.updatedItemTitle;
+    const response = await db.query("UPDATE items SET title = $1 WHERE id = $2", [updatedItemTitle, updatedItemId]);
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+    if (error.code == 22001) {
+      res.status(500).json("Value too long, maximum 100 characters");
+    } else {
+      res.status(500).json("Internal server error");
+    }
+  }
+});
+
+app.post("/delete", async (req, res) => {
+  console.log(req.body);
+  try {
+    const deleteItemId = req.body.deleteItemId;
+    const response = await db.query("DELETE FROM items WHERE id = $1", [deleteItemId]);
     res.redirect("/");
   } catch (error) {
     console.log(error);
     res.status(500).json("Internal server error");
   }
-  items.push({ title: newItem });
-  res.redirect("/");
-});
-
-app.post("/edit", (req, res) => {
-  console.log(req.body);
-  const updatedItemId = req.body.updatedItemId;
-  const updatedItemTitle = req.body.updatedItemTitle;
-});
-
-app.post("/delete", (req, res) => {
-  console.log(req.body);
 });
 
 app.listen(port, () => {
